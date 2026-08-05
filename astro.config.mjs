12ccb1816b95d44b84ebb7d367b946e75f232480
gina <gina@ginas-MacBook-Air-4.local>
2026-08-05 11:11:18 -0700

    Add TinaCMS admin editing workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
+import tina from "@tinacms/astro/integration";
+import { tinaAdminDevRedirect } from "@tinacms/astro/vite";
 
 export default defineConfig({
+  adapter: vercel(),
   devToolbar: {
     enabled: false
   },
-  output: "static"
+  integrations: [tina()],
+  output: "static",
+  trailingSlash: "always",
+  vite: {
+    plugins: [tinaAdminDevRedirect()]
+  }
 });
